d8db394fc58b5c61d49db32fe9efe23382061ada
Zhiyun Ren <zyren00@gmail.com>
2016-11-26 23:43:06 -0500

    hw1 p1

--- p1.py
+++ p1.py
@@ -1,3 +1,16 @@
+import math
 def joint_force(lengths, thetas, masses):
     print(lengths, thetas, masses)
-    return 0, 0
+    m1 = masses[0]
+    m2 = masses[1]
+    g = 9.8
+    l1 = lengths[0]
+    l2 = lengths[1]
+    t1 = thetas[0]
+    t2 = thetas[1]
+    tmp = m2 * l2 / 2.0 * math.cos(t1 + t2)
+    tau1 = ((m1 / 2.0 + m2) * l1 * math.cos(t1) + tmp) * g
+    tau2 = tmp * g
+    print (tau1, tau2)
+    # use the scaling factor of -1.93 on the piazza post
+    return (-1.93 * tau1, -1.93 * tau2)
